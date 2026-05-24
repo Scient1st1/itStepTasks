e0a2e371bc24bc90812a82c588258e7b75ff2d2a
Levan Tchumburidze <40963691+Scient1st1@users.noreply.github.com>
2026-05-25 03:07:59 +0400

    added two additional tasks

--- assets/script.js
+++ assets/script.js
@@ -20,6 +20,14 @@ document.addEventListener("DOMContentLoaded", () => {
       title: "Task 5 - Apply Form",
       src: "tasks/task5/index.html",
     },
+    {
+      title: "Task 6.1 - Grid during lesson",
+      src: "tasks/task6/practical.html",
+    },
+    {
+      title: "Task 6.2 - OCEANPETS Template",
+      src: "tasks/task6/index.html",
+    },
   ];
   const taskList = document.querySelector(".task-list");
 
